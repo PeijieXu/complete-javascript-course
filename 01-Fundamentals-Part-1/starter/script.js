6f9e13db7f2ccaca62c7533c67dfb0693a6c8cba
peijie <peijiexu99@gmail.com>
2022-05-26 14:48:20 -0700

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,39 @@
-let js = 'amazing';
-if (js == 'amazing') alert('javascript is FUN?');
 
-console.log(40 + 8 + 23);
+
+// primative data types
+let age = 22; // 
+let firstName = 'Jonas';
+let isAdult = true;
+
+let children;
+
+/*
+// js has dynamic typing
+// value is NOT variable
+console.log(firstName);
+console.log(typeof isAdult);
+
+console.log(typeof children);
+
+console.log(typeof null);
+
+// use const keyword
+const PI = 3.14159;
+console.log(PI);
+
+// use var keyword
+var job = 'programmer';
+job = 'teacher';
+
+console.log(`the PI is 
+${PI}
+`);
+*/
+
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+  console.log(`you can drive now 👍`);
+} else {
+  console.log(`nope 😢`);
+}
+// finished Type Conversion and Coercion
